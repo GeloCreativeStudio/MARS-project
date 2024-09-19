086f9318435f8eda59bea655a8df9336899ee988
Angelo Manalo <manaloangelo1017@gmail.com>
2024-09-19 20:40:19 +0800

    Deploy

--- app.py
+++ app.py
@@ -196,4 +196,4 @@ def serve_assets(filename):
 
 if __name__ == "__main__":
     logger.info("Flask app started")
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
